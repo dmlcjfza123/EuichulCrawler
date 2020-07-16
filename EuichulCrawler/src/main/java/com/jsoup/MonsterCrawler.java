e2860e2f551d47aa94666a052bb2d9d4e8df77af
JungEuiChul <dmlcjfza123@gmail.com>
2020-07-16 15:35:30 +0900

    branch 독립성 확인 commit 3

--- EuichulCrawler/src/main/java/com/jsoup/MonsterCrawler.java
+++ EuichulCrawler/src/main/java/com/jsoup/MonsterCrawler.java
@@ -57,7 +57,8 @@ import com.dto.MonsterNormalSkillDto;
 import com.sun.jdi.connect.spi.Connection;
 import com.util.TranslateText;
 
-//test
+//test branch 1
+//test branch 2
 
 //@Component
 public class MonsterCrawler {
